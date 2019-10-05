364a3bb95329cdf7b7a717adc2dc8005dcc3b265
ppo01 <55887180+ppo01@users.noreply.github.com>
2019-10-05 22:03:55 +0200

    Add files via upload

---
+++ valtozocsere_szorzat.cpp
@@ -0,0 +1,25 @@
+#include"std_lib_facilities.h"
+using namespace std;
+
+int main()
+{
+	int a = 0, b = 0;
+	
+	cout << "Adjon meg ket szamot:\n";
+	cin >> a;
+	cin >> b;
+
+	cout << "\nElso valtozo= " << a << "\n";
+	cout << "Masodik valtozo= " << b << "\n\n";
+
+	a = a * b;
+	b = a / b;
+	a = a / b;
+
+	cout << "Csere utan:\n"
+		"a= " << a << "\n"
+		"b=" << b << "\n";
+
+
+	return 0;
+}
